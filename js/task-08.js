ebf827421b38e32f9a97d31706be23cc6c5c1f4c
Alona1366 <olena1366@gmail.com>
2022-10-26 20:42:59 +0300

    create task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+        alert(`Всі поля повинні бути заповнені`)
+    }
+
+    console.log(`mail: ${email.value}, password: ${password.value}`);
+    event.currentTarget.reset();
+    }
