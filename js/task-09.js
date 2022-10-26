8076545084ce20ec07d19090029e5019b2a90ab1
Alona1366 <olena1366@gmail.com>
2022-10-26 20:43:10 +0300

    create task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanEl = document.querySelector('.color');
+const btnEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+
+btnEl.addEventListener('click', changeColor);
+
+function changeColor() {
+const color = getRandomHexColor();
+bodyEl.setAttribute('style', `background-color: ${color}`);
+spanEl.textContent = color;
+}
