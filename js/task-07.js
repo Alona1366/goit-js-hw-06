d4ec08e2fb86e1af32931294a0e720858922c26f
Alona1366 <olena1366@gmail.com>
2022-10-31 17:03:11 +0200

    task 7 fixes

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,19 @@
-const inputTypeRange = document.querySelector('#font-size-control');
+// const inputTypeRange = document.querySelector('#font-size-control');
 
-const spanText = document.querySelector('#text');
-spanText.style.fontSize = `${Number(inputTypeRange.value)}px`;
+// const spanText = document.querySelector('#text');
+// spanText.style.fontSize = `${Number(inputTypeRange.value)}px`;
 
-console.log(spanText.style);
+// console.log(spanText.style);
+
+const refs = {
+    input: document.querySelector('input'),
+    span: document.querySelector('span'),
+}
+
+refs.span.classList.add('text-style');
+
+refs.input.addEventListener('input', onInputChange);
+
+function onInputChange(event) {
+    refs.span.style.fontSize = `${event.currentTarget.value}px`
+}
